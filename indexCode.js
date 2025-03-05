ad2644dee58f6aa2f9010e217a9fd88aa2aec24b
BrandonSchuitema <jrandon1129@gmail.com>
2025-03-04 23:57:05 -0500

    More Additions!
    I was trying to figure out how I could make alerts into popups, but I don't think I like that method. I might not even make a review text system and only add in images of books. Maybe something just for a rating. I also added a few more books into the site.

--- indexCode.js
+++ indexCode.js
@@ -1,11 +1,36 @@
 function ShowReview(){
-    document.getElementById("review1").innerHTML = "Hello World!";
+    /*document.getElementById("review1").innerHTML = "Hello World!";*/
+    alert("This book/ manga was good, but I have too many reviews to write and so I made this generic response.");
 }
 
+
+/*Book Club*/
+function SilentPatientReview(){
+    alert("Honestly this book wasn't that good. I enjoyed it, but it was a 2.5/ 5 for me. It could've been so much better with just a few tweaks.");
+}
+function FiveSurviveReview(){
+    alert("Holly Jackson has a super power for endings. She also writes really good and entertaining books.");
+}
+function SevenYearReview(){
+    alert("It was fine, but it could've been better if it was written by somebody else. I don't think the author was good at writing.");
+}
+
+
+/*Overlord*/
 function ShowOverlordVol2Review(){
-    document.getElementById("review2").innerHTML = "This book was of course amazing. It's Overlord after all.";
+    /*document.getElementById("review2").innerHTML = "This book was of course amazing. It's Overlord after all.";*/
+    alert("Overlord Volume 2 was amazing!");
+}
+function ShowOverlordVol3Review(){
+    /*document.getElementById("review3").innerHTML = "Hello";*/
+    alert("Overlord Volume 3 was tremendous!");
 }
 
-function ShowOverlordVol3Review(){
-    document.getElementById("review3").innerHTML = "Hello";
+
+/*Monster Girls*/
+function MGEncyclopediaReview(){
+    alert("I love this book. And not for the reason you think. It's the world building, and the characters all living happy lives. It's fun to read.")
+}
+function MG2EncyclopediaReview(){
+    alert("I love this book for the same reasons as the first one. Reading this also really makes me want a volume 3. Hopefully that comes in my lifetime. Also why does the government not allow translations of his other books, but allow very sexual romance novels. Makes no sense to me.")
 }
