50e2692b54ec263868d2cedd57ebca3210ffa416
BrandonSchuitema <jrandon1129@gmail.com>
2025-03-04 21:42:30 -0500

    Visuals and Buttons!
    I added in a pulse glow effect. That was cool. And I'm trying to figure out how I can make a review pop up when the user presses a button.

---
+++ indexCode.js
@@ -0,0 +1,11 @@
+function ShowReview(){
+    document.getElementById("review1").innerHTML = "Hello World!";
+}
+
+function ShowOverlordVol2Review(){
+    document.getElementById("review2").innerHTML = "This book was of course amazing. It's Overlord after all.";
+}
+
+function ShowOverlordVol3Review(){
+    document.getElementById("review3").innerHTML = "Hello";
+}
